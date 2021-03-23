ef296cf505cb816226edd8d955992931a8e0ad79
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-03-23 20:29:23 -0300

    Added social login with githib

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,18 +2,29 @@ import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
+import { signIn, signOut, useSession } from "next-auth/client";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const signed = true;
-  return signed ? (
-    <button type="button" className={styles.buttonContainer}>
+  const [session] = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.buttonContainer}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04D361" size={24} />
-      maykonoliveira
+      {session.user.name}
       <FiX color="#737380" size={24} className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.buttonContainer}>
+    <button
+      type="button"
+      className={styles.buttonContainer}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#EBA417" size={24} />
       Sign in with Github
     </button>
