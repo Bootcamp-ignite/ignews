61f46397d26aea4c39855fb165d407753500b201
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-03-26 18:53:21 -0300

    Add subscribe checkout with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
+import { signIn, useSession } from "next-auth/client";
 import React from "react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripeJs";
 
 import styles from "./styles.module.scss";
 
@@ -7,8 +10,33 @@ interface SubscribeButtonProps {
 }
 
 export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const session = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.buttonContainer}>
+    <button
+      type="button"
+      className={styles.buttonContainer}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
